9a7cbade39193fd8f106bc0da70272ed12d681ad
Dhruv-V <79684877+Dhruv-V@users.noreply.github.com>
2026-04-20 16:09:06 +0530

    feat: feat: Folder Structure

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import EmailSignup from './components/EmailSignup';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <EmailSignup />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
